69f6e87ccb95f702664347345f9d2744d34a2878
Shane Nelson <swiftyshane@gmail.com>
2017-05-22 00:09:12 -0400

    need to finish open channel view controller.

--- LANLineV2/OpenChannelListTableViewCell.swift
+++ LANLineV2/OpenChannelListTableViewCell.swift
@@ -8,17 +8,20 @@
 
 import UIKit
 
-class OpenChannelListTableViewCell: UITableViewCell {
+class OpenChannelListTableViewCell: UITableViewCell
+{
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+  override func awakeFromNib()
+  {
+      super.awakeFromNib()
+      // Initialization code
+  }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+  override func setSelected(_ selected: Bool, animated: Bool)
+  {
+      super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
-    }
+      // Configure the view for the selected state
+  }
 
 }
