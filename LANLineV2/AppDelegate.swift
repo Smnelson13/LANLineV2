fe68e845c6e27aed21b3b25cc9a51ac9879101fb
Shane Nelson <swiftyshane@gmail.com>
2017-05-30 16:50:47 -0400

    colors

--- LANLineV2/AppDelegate.swift
+++ LANLineV2/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
   {
+    UIApplication.shared.statusBarStyle = .lightContent
     let notificationSettings = UIUserNotificationSettings(types: [UIUserNotificationType.alert, UIUserNotificationType.badge, UIUserNotificationType.sound], categories: nil)
     UIApplication.shared.registerUserNotificationSettings(notificationSettings)
     UIApplication.shared.registerForRemoteNotifications()
