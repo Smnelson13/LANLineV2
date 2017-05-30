f2a5520b825315b0d450f0a11b62aff8b5af11d3
Shane Nelson <swiftyshane@gmail.com>
2017-05-30 18:57:54 -0400

    working on animated tab bar

--- LANLineV2/AppDelegate.swift
+++ LANLineV2/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
   {
+    UITabBar.appearance().tintColor = .white
+    UITabBar.appearance().unselectedItemTintColor = .white
     UIApplication.shared.statusBarStyle = .lightContent
     let notificationSettings = UIUserNotificationSettings(types: [UIUserNotificationType.alert, UIUserNotificationType.badge, UIUserNotificationType.sound], categories: nil)
     UIApplication.shared.registerUserNotificationSettings(notificationSettings)
